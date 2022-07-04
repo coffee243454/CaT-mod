98df1148f0099f054ea77c1b574668311184c2fc
coffee243454 <35109746+coffee243454@users.noreply.github.com>
2022-07-04 21:52:35 +0100

    Fix build, add gold and tritite, fix textures, add lang files, W commit

--- src/main/java/net/coffee/mod/coffeemod.java
+++ src/main/java/net/coffee/mod/coffeemod.java
@@ -18,11 +18,18 @@ public class coffeemod implements ModInitializer {
 
     public static final Block POG_BLOCK = new pogblock();
 
+    public static final Block TRIRITE_BLOCK = new pogblock();
+
+    public static final Item GOLD_TRIDENT = new Item(new FabricItemSettings().group(ItemGroup.COMBAT));
+
     @Override
     public void onInitialize() {
         Registry.register(Registry.ITEM, new Identifier("coffeemod", "coffee"), COFFEE_ITEM);
         Registry.register(Registry.BLOCK, new Identifier("coffeemod", "pogblock"), POG_BLOCK);
         Registry.register(Registry.ITEM, new Identifier("coffeemod", "pogblock"), new BlockItem(POG_BLOCK, new  Item.Settings().group(ItemGroup.MISC)));
+        Registry.register(Registry.BLOCK, new Identifier("coffeemod", "tririteblock"), TRIRITE_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("coffeemod", "tririteblock"), new BlockItem(TRIRITE_BLOCK, new  Item.Settings().group(ItemGroup.MISC)));
+        Registry.register(Registry.ITEM, new Identifier("coffeemod", "goldtrident"), GOLD_TRIDENT);
      }
      
 }
