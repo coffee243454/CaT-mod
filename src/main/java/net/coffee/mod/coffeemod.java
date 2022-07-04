275c9799063570093c15328b353f9ecec472c852
coffee243454 <35109746+coffee243454@users.noreply.github.com>
2022-07-04 17:50:53 +0100

    adding pogblock and pixes

--- src/main/java/net/coffee/mod/coffeemod.java
+++ src/main/java/net/coffee/mod/coffeemod.java
@@ -2,6 +2,8 @@ package net.coffee.mod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.registry.Registry;
@@ -12,19 +14,15 @@ import net.minecraft.util.Identifier;
 
 public class coffeemod implements ModInitializer {
 
-    public static final Item COFFEE_ITEM = new CoffeeItem(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item COFFEE_ITEM = new CoffeeItem(new FabricItemSettings().group(ItemGroup.FOOD));
 
-    public static final Item GOLD_TRIDENT = new Item(new FabricItemSettings().group(ItemGroup.MISC));
-    
-    public static final Item TRIRITE_BLOCK = new BlockItem(TRIRITE_BLOCK,new FabricItemSettings().group(ItemGroup.MISC));
-
+    public static final Block POG_BLOCK = new pogblock();
 
     @Override
     public void onInitialize() {
         Registry.register(Registry.ITEM, new Identifier("coffeemod", "coffee"), COFFEE_ITEM);
-        Registry.register(Registry.ITEM, new Identifier("coffeemod", "gold_trident"), GOLD_TRIDENT);
-        Registry.register(Registry.ITEM, new Identifier("coffeemod", "tririte_block"), TRIRITE_BLOCK);
-
+        Registry.register(Registry.BLOCK, new Identifier("coffeemod", "pogblock"), POG_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("coffeemod", "pogblock"), new BlockItem(POG_BLOCK, new  Item.Settings().group(ItemGroup.MISC)));
      }
      
 }
